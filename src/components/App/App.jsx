69b462c990119a1e5ec2aa36fec93ec0ce5e2282
matthaeuss <91155894+matthaeuss@users.noreply.github.com>
2022-05-21 14:59:01 +0200

    navbar functionalities on the main page

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,14 +2,19 @@ import React from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 import './App.scss';
+import Header from '../Header/Header';
+import Train from '../Train/Train';
+import Profile from '../Profile/Profile';
 
 function App() {
     return (
         <BrowserRouter>
-            <div>
-                <Route path="/" element={<Link to="/Profile">Profile</Link>} />
-                <Route path="/profile" element={<Link to="/Profile">Home</Link>} />
-            </div>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Link to="/profile">Profile</Link>} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/train" element={<Train />} />
+            </Routes>
         </BrowserRouter>
     );
 }
