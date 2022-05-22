fa7d73875c5300d11a2b6371e11eced553a1f21f
matthaeuss <91155894+matthaeuss@users.noreply.github.com>
2022-05-22 10:26:19 +0200

    login component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,34 +7,27 @@ import Header from '../Header/Header';
 import Profile from '../Profile/Profile';
 import Train from '../Train/Train';
 import './App.scss';
+import { Col, Container, Row } from 'react-bootstrap';
 
 function App() {
     const [cookie, setCookie] = useCookie('token_auth');
-    const navigate = useNavigate();
-
-    useEffect(() => {
-        if (cookie === undefined) {
-            navigate('/login');
-        }
-    }, []);
 
     return (
         <BrowserRouter>
             <Header />
-            <Routes>
-                {cookie ? (
-                    <>
-                        <Route path="/" element={<Link to="/profile">Profile</Link>} />
-                        <Route path="/profile" element={<Profile />} />
-                        <Route path="/train" element={<Train />} />
-                    </>
-                ) : (
-                    <>
-                        <Route path="/login" element={<Login />} />
-                        <Route path="/register" element={<Register />} />
-                    </>
-                )}
-            </Routes>
+            <Container>
+                <Row>
+                    <Col>
+                        <Routes>
+                            <Route path="/" element={<Link to="/profile">Profile</Link>} />
+                            <Route path="/profile" element={<Profile />} />
+                            <Route path="/train" element={<Train />} />
+                            <Route path="/login" element={<Login />} />
+                            <Route path="/register" element={<Register />} />
+                        </Routes>
+                    </Col>
+                </Row>
+            </Container>
         </BrowserRouter>
     );
 }
