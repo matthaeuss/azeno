a4bef4c7dee5f399a049c02c9d5477a095c01802
matthaeuss <91155894+matthaeuss@users.noreply.github.com>
2022-05-22 10:44:19 +0200

    custom color for bootstrap

--- src/components/Auth/Register.jsx
+++ src/components/Auth/Register.jsx
@@ -1,10 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, Card, Form } from 'react-bootstrap';
 
 function Register() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [remember, setRemember] = useState(false);
+
+    function handleEmail(event) {
+        setEmail(event.target.value);
+    }
+
+    function handlePassword(event) {
+        setPassword(event.target.value);
+    }
+
+    function submitForm(event) {
+        event.preventDefault();
+        console.log(email, password, remember);
+    }
+
     return (
-        <div>
-            <h2>Register</h2>
-        </div>
+        <Card>
+            <Card.Body>
+                <Card.Title>Register</Card.Title>
+                <Form onSubmit={submitForm} className="d-flex flex-column">
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control
+                            type="email"
+                            placeholder="Enter email"
+                            value={email}
+                            onChange={handleEmail}
+                            className={'Eluwina'}
+                        />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control
+                            type="password"
+                            placeholder="Password"
+                            value={password}
+                            onChange={handlePassword}
+                        />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Register
+                    </Button>
+                </Form>
+            </Card.Body>
+        </Card>
     );
 }
 
