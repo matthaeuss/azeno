7dfc53e888c68c8dcd0b385f54f783fccedd5157
matthaeuss <91155894+matthaeuss@users.noreply.github.com>
2022-05-22 09:29:00 +0200

    first implementation of guard

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,19 +1,39 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-
-import './App.scss';
+import React, { useEffect } from 'react';
+import { BrowserRouter, Link, Route, Routes, useNavigate } from 'react-router-dom';
+import Login from '../Auth/Login';
+import Register from '../Auth/Register';
+import useCookie from '../../hooks/useCookie';
 import Header from '../Header/Header';
-import Train from '../Train/Train';
 import Profile from '../Profile/Profile';
+import Train from '../Train/Train';
+import './App.scss';
 
 function App() {
+    const [cookie, setCookie] = useCookie('token_auth');
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (cookie === undefined) {
+            navigate('/login');
+        }
+    }, []);
+
     return (
         <BrowserRouter>
             <Header />
             <Routes>
-                <Route path="/" element={<Link to="/profile">Profile</Link>} />
-                <Route path="/profile" element={<Profile />} />
-                <Route path="/train" element={<Train />} />
+                {cookie ? (
+                    <>
+                        <Route path="/" element={<Link to="/profile">Profile</Link>} />
+                        <Route path="/profile" element={<Profile />} />
+                        <Route path="/train" element={<Train />} />
+                    </>
+                ) : (
+                    <>
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/register" element={<Register />} />
+                    </>
+                )}
             </Routes>
         </BrowserRouter>
     );
--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,8 @@ module.exports = {
     },
     // webpack 5 comes with devServer which loads in development mode
     devServer: {
-        port: 3001
+        port: 3001,
+        historyApiFallback: true
         // watchContentBase: true
     },
     // Rules of how webpack will take our files, complie & bundle them for the browser
